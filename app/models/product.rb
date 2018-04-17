b0847138902e3b0db7b722ea062b8645bcd9b4b9
Le Luong-Thien <lelt@pc-254.local.elarion.com>
2018-04-17 13:07:49 +0700

    model test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-    validates :title, :description, :image_url, presence: true        
+    validates :description, :image_url, presence: true        
+    validates :title, uniqueness: true, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
     validates :image_url, allow_blank: true, format: {
         with: %r{\.(gif|jpg|png)\Z}i,
