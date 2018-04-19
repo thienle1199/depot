22f33b1dc235f4be71796f03bc2027b2cfa8b185
Le Luong-Thien <lelt@pc-254.local.elarion.com>
2018-04-19 15:27:58 +0700

    implemeted mailing and integration test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'thienle1144@gmail.com',
+      :password             => '875849Luong',
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  }
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  default_url_options :host => "localhost:3000"
   resources :orders
   resources :line_items
   resources :carts
